69cbf8e36a1daabe21069fe594cc7724939e1175
mrc <marco.capano@pushapp.me>
2018-10-18 18:22:47 +0200

    Initial Commit

---
+++ TypeSafePassword/TypeSafePassword.h
@@ -0,0 +1,19 @@
+//
+//  TypeSafePassword.h
+//  TypeSafePassword
+//
+//  Created by Marco Capano on 18/10/2018.
+//  Copyright © 2018 Marco Capano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TypeSafePassword.
+FOUNDATION_EXPORT double TypeSafePasswordVersionNumber;
+
+//! Project version string for TypeSafePassword.
+FOUNDATION_EXPORT const unsigned char TypeSafePasswordVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TypeSafePassword/PublicHeader.h>
+
+
